1d87d449a0751399804ecf49ead4a792df938c88
Gitlab CI/CD <infra_ci@innovaccer.com>
2026-07-04 04:44:03 +0530

    add user model schema

---
+++ server/models/user_model.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timezone
+from enum import Enum
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class Role(str, Enum):
+    admin = "admin"
+    partner = "partner"
+    user = "user"
+
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: Role = Role.user
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
