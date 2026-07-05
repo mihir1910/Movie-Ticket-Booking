ace5a01acddcc35f4032a88b08f4639f3226849a
Gitlab CI/CD <infra_ci@innovaccer.com>
2026-07-06 00:39:00 +0530

    auth calls and cnfig

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './pages/Home'
 import Login from './pages/Login';
 import Register from './pages/Register';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
